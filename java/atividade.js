0c32523b5fe214d051145ad4e9bef79c7f2e53cb
adrianhermes101 <128698958+adrianhermes101@users.noreply.github.com>
2024-02-23 11:58:29 -0300

    Add files via upload

---
+++ java/atividade.js
@@ -0,0 +1,8 @@
+let nome = "gelson";
+let idade = 68;
+let peso = 122;
+let altura = 1.50;
+let numero = 8
+
+
+console.log(`o nome do sujeito is ${nome} e a ${idade}, o peso do bixo é ${peso} e altura é ${altura}.Agora caso some a idade do bixaum mais por exemplo o 8 fica ${idade + numero}`);
