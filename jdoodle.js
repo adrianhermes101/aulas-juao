f1c48e36bf2c22c7fcb8764a4e75701a3e58b5ce
adrianhermes101 <128698958+adrianhermes101@users.noreply.github.com>
2024-03-01 12:08:54 -0300

    Add files via upload

---
+++ jdoodle.js
@@ -0,0 +1,39 @@
+let nome = `Adrian`;
+let idade = 17;
+let altura = 1.68;
+let casado = false; 
+
+console.log(nome, idade ,altura, casado)  
+
+let pessoa = {
+    nome: `Adrian`,
+    idade: 17,
+    casado: false,
+    altura: 1.68,
+} 
+
+console.log(pessoa.nome, pessoa.idade, pessoa.casado, pessoa.altura)
+
+let array = [14,54,43,67,87]
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+
+let soma = 2 + 4;
+let subtracão = 67-78;
+let multiplicacao = 56 * 2;
+let divisao = 34/2;
+let restoDivisao = 12 % 3;
+console.log(soma, subtracão, multiplicacao, divisao, restoDivisao)
+
+let soma1 = 175 + 175 
+let soma2 = 350 + 5000
+let soma3 = 5350 + 10000
+
+console.log(soma3.toFixed((3))
+
+
+
